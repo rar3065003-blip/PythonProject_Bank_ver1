d5bc0721ec851a280b40a8b867241365296e9654
Андрей Атаманов <rar3065003@gmail.com>
2025-12-20 21:51:00 +0700

    delete test.py do not use more

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from src.widget import mask_account_card
 
 if __name__ == "__main__":
-    result_1= mask_account_card('Maestro asdfhg gfhfgh 1596837868705199')
+    result_1= mask_account_card('Maestro 1596837868705199')
     result_2 = mask_account_card('Счет 73654108430135874305')
     print(result_1 )
     print(result_2 )
