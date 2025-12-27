efa7bee2a82489c25ce23c30bd3ba375b4b9d095
Андрей Атаманов <rar3065003@gmail.com>
2025-12-27 19:11:12 +0700

    заполнение информации по проекту

--- src/processing.py
+++ src/processing.py
@@ -23,18 +23,14 @@ print(result_1)
 
 from datetime import datetime
 
-
-def sort_by_date(date_calculate: list, reverse = False) -> list:
+def sort_by_date(date_calculate: list, reverse: bool = False) -> list:
     """Принимает список словарей и порядок сортировки возвращает новый список с сортировкой по дате"""
-    result_sort = []
-    for item in date_calculate:
-        result_sort = sorted(item['date'])
 
-    return result_sort
+    return sorted(date_calculate, key = lambda item:item['date'], reverse = reverse)
 
 
 result_2 = sort_by_date([{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
 {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
  {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
- {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}])
+ {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}], True)
 print(result_2)
