06f835a1fd2b9299c505a25cab372b750525161e
Андрей Атаманов <rar3065003@gmail.com>
2025-12-27 19:19:36 +0700

    изменения в модуле

--- src/processing.py
+++ src/processing.py
@@ -29,8 +29,3 @@ def sort_by_date(date_calculate: list, reverse: bool = False) -> list:
     return sorted(date_calculate, key = lambda item:item['date'], reverse = reverse)
 
 
-result_2 = sort_by_date([{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
-{'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
- {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
- {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}], True)
-print(result_2)
