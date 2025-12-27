90a5c2b521446be5ff60a39900687b8c8977d7e6
Андрей Атаманов <rar3065003@gmail.com>
2025-12-27 18:06:05 +0700

    working by sort_by date

--- src/processing.py
+++ src/processing.py
@@ -21,8 +21,20 @@ result_1 = filter_by_state([{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-
  {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}])
 print(result_1)
 
+from datetime import datetime
 
-def sort_by_date(date_calculate: list, sort = True) -> list:
+
+def sort_by_date(date_calculate: list, reverse = False) -> list:
     """Принимает список словарей и порядок сортировки возвращает новый список с сортировкой по дате"""
+    result_sort = []
+    for item in date_calculate:
+        result_sort = sorted(item['date'])
 
-    pass
+    return result_sort
+
+
+result_2 = sort_by_date([{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
+{'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
+ {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
+ {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}])
+print(result_2)
