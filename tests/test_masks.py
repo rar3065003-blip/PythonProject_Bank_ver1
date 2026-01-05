8232b982a35efd340e05d14be7570206069cd54e
Андрей Атаманов <rar3065003@gmail.com>
2026-01-05 22:17:09 +0700

    create conftest

--- tests/test_masks.py
+++ tests/test_masks.py
@@ -1,20 +1,32 @@
+from src.masks import get_mask_card_number, get_mask_account
 import pytest
 
-def test_get_mask_card_number(numbers_1: int) -> str:
+
+def test_get_mask_card_number_correct_mask(numbers_1: int) -> str:
       pass
 
-
-
-def test_get_mask_account(account_1: int) -> str:
+def test_get_mask_card_number_correct_length(numbers_1: int) -> str:
       pass
 
+def test_get_mask_card_number_correct_digits(numbers_1: int) -> str:
+     pass
+
+def test_get_mask_card_number_no_number(numbers_1: int) -> str:
+     pass
 
 
 
+def test_get_mask_account_correct_mask(account_1: int) -> str:
+      pass
 
+def test_get_mask_account_correct_length(account_1: int) -> str:
+      pass
 
+def test_get_mask_account_correct_digits(account_1: int) -> str:
+      pass
 
-
+def test_get_mask_account_no_numbers(account_1: int) -> str:
+      pass
 
 
 # Тестирование правильности маскирования номера карты.
