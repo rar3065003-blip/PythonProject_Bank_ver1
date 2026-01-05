1439344e19737681584321a1e86f4ab3ccda8ac1
Андрей Атаманов <rar3065003@gmail.com>
2026-01-05 21:54:13 +0700

    create tests 3 category items

---
+++ tests/test_processing.py
@@ -0,0 +1,8 @@
+import pytest
+
+
+def test_filter_by_state(user_calculate: list, state: str = "EXECUTED") -> list:
+    pass
+
+def test_sort_by_date(date_calculate: list, reverse: bool = True) -> list:
+    pass
---
+++ tests/test_widget.py
@@ -0,0 +1,9 @@
+import pytest
+
+def test_mask_account_card(user_card: str) -> str:
+
+    pass
+
+def test_get_date(date_user: str) -> str:
+
+    pass
