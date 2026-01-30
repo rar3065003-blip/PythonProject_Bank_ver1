aac16f9954f89105e05452443a3e9c7e14da1d28
Андрей Атаманов <rar3065003@gmail.com>
2026-01-30 22:21:17 +0700

    redacting redme

--- src/decorators.py
+++ src/decorators.py
@@ -1,35 +1,35 @@
 import time
 import functools
 from collections.abc import Callable
+from typing import Any
 
 
-def log(filename = None):
-    def decorator(func: Callable):
+def log(filename: Any = None) -> Callable:
+    """ Функция записи лога выполнения исполняемой функции -
+     сохраняет в лог в файл при задании имени или выводит в консоль"""
+    def decorator(func: Callable) -> Callable:
+        """Принимает вызов функции"""
         @functools.wraps(func)
-        def wrapper(*args, **kwargs):
+        def wrapper(*args: tuple, **kwargs: dict) -> Any:
+            """Оборачиваем функцию и запускаем запись и вычисление времени работы"""
             start = time.time()
-            msg = f'{start}ms {func.__name__} started \n'
+            msg = f"{start}ms {func.__name__} started \n"
             result = None
             try:
                 result = func(*args, **kwargs)
             except Exception as w:
-                msg += f'{func.__name__} raised with arguments {args, kwargs} but it didn`t worked, error:{str(w)} \n'
+                msg += f"{func.__name__} raised with arguments {args, kwargs} but it didn`t worked, error:{str(w)} \n"
             finally:
                 finish = time.time()
                 raise_time = finish - start
-                msg += f'{raise_time}ms {func.__name__} finished'
+                msg += f"{raise_time}ms {func.__name__} finished"
                 if filename:
-                    with open(filename, mode = "a", encoding = "utf-8") as x:
+                    with open(filename, mode="a", encoding="utf-8") as x:
                         x.write(msg + "\n")
                 else:
                     print(msg)
             return result
 
         return wrapper
+
     return decorator
-
-
-
-
-
-
--- tests/test_decorators.py
+++ tests/test_decorators.py
@@ -1,8 +1,8 @@
-from decorators import log
+from src.decorators import log
 
 
-def test_log():
+def test_log() -> None:
     @log()
     @log("pivas.txt")
-    def delite(a,b):
+    def delite(a: int, b: int) -> float:
         return a / b
