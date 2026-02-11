332a0ed81fb6746f796d3e13f054eb0f9f308022
Андрей Атаманов <rar3065003@gmail.com>
2026-02-11 18:46:57 +0700

    reate loggers into masks and utils

--- src/masks.py
+++ src/masks.py
@@ -1,7 +1,26 @@
+import logging
+
+
+def setup_logging_masks():
+    logging.basicConfig(level=logging.DEBUG,
+                        format= '%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                        filename ='application.log',
+                        filemode='w',
+                        encoding='utf-8')
+    global get_mask_card_number_logger, get_mask_account_logger
+
+    get_mask_card_number_logger = logging.getLogger('app.get_mask_card_number')
+    get_mask_account_logger = logging.getLogger('app.get_mask_account')
+
+setup_logging_masks()
+
 def get_mask_card_number(numbers_1: int) -> str:
     """Принимает на вход номер карты и возвращает маску номера карты"""
+    get_mask_card_number_logger.info("Сообщение о запуске функции get_mask_card_number")
+
     numbers = str(numbers_1)
     if len(numbers) != 16:
+        get_mask_card_number_logger.error("Не корректная длинна номера карты, %d", len(numbers))
         return "Введите 16 цифр"
     else:
         part_1 = list(numbers[:4])
@@ -21,8 +40,10 @@ def get_mask_card_number(numbers_1: int) -> str:
 
 def get_mask_account(account_1: int) -> str:
     """Функция принимает номер счета, выводит маску номера счета типа **ХХХХ"""
+    get_mask_account_logger.info("Запуск функции get_mask_account")
     account = str(account_1)
     if len(account) != 20:
+        get_mask_account_logger.error("Не корректная длинна номера карты, %d", len(account))
         return "Введите  номер счета 20 цифр"
     else:
         part_account = account[-4:]
@@ -30,3 +51,9 @@ def get_mask_account(account_1: int) -> str:
         part_sum = part_mask + part_account
 
     return part_sum
+
+
+result = get_mask_account(1234568912345678978)
+print(result)
+result_2 = get_mask_card_number(123456891234567)
+print(result_2)
--- src/utils.py
+++ src/utils.py
@@ -1,13 +1,28 @@
 import json
 import os
 from json import JSONDecodeError
-
+import logging
 from config import ROOT_DIR
 from src import external_api
+
+def setup_logging_utils():
+    logging.basicConfig(level=logging.DEBUG,
+                        format= '%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                        filename ='application.log',
+                        filemode='w')
+    global dict_transactions_logs, convertation_currency_logs
+
+    # логеры компонентов функций
+    # директория utils
+    dict_transactions_logs = logging.getLogger('app.dict_transactions')
+    convertation_currency_logs = logging.getLogger('app.convertation_currency')
+
+setup_logging_utils()
 
 
 def dict_transactions(file_json_dict: str) -> list[dict]:
     """Принимает json файл и возвращает список транзакций в формате *.py"""
+    dict_transactions_logs.info("Запуск функции dict_transactions")
     list_result: list = []
     path_filename: str = f"{ROOT_DIR}{file_json_dict}"
     file_exist: bool = os.path.exists(path_filename)
@@ -26,6 +41,7 @@ def dict_transactions(file_json_dict: str) -> list[dict]:
 def convertation_currency(transaction: dict) -> float:
     """Принимает транзакцию в виде словаря и возвращает сумму транзакции,
     конвертированную в рубли по курсу на сегодняшний день, либо просто в рублях"""
+    convertation_currency_logs("Запуск функции convertation_currency")
     currency: str = transaction.get("operationAmount", {}).get("currency", {}).get("code", "")
     amount: float = float(transaction.get("operationAmount", {}).get("amount", 0))
     if currency == "RUB":
