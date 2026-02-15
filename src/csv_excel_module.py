cfe56799d3b2bea34698c09393493dbc9ccce9c5
Андрей Атаманов <rar3065003@gmail.com>
2026-02-16 00:02:47 +0700

    create csv module complite

--- src/csv_excel_module.py
+++ src/csv_excel_module.py
@@ -1,5 +1,32 @@
-def csv_module():
-    pass
+from typing import Any
 
-def excel_module():
-    pass
+import pandas as pd
+import csv
+import openpyxl
+
+# Функция должна возвращать данные в виде структуры данных,
+# например, списка словарей, а не выводить их в консоль.
+# Такая структура позволит тебе легко работать с данными в дальнейшем.
+# Возвращать данные в виде файла не требуется,
+# так как целью является обработка и анализ информации,
+# а не её повторная запись в файл.
+
+def csv_module(df)-> dict[str | Any, str | Any] | None:
+    with open(df, newline='', encoding = 'utf-8') as file:
+        reader = csv.DictReader(file)
+        for row in reader:
+            return row
+
+result = csv_module('../data/transactions.csv')
+print(result)
+print(type(result))
+
+def excel_module(df, user_request):
+    reader = pd.read_excel(df, engine="openpyxl")
+    result_2 = reader[user_request]
+    return result_2
+
+result = excel_module('../data/transactions_excel.xlsx', 'state')
+print(result)
+print(type(result))
+
