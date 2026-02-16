af5794156b703ad648d82a2c3808f8b280eb6cea
Андрей Атаманов <rar3065003@gmail.com>
2026-02-16 19:18:38 +0700

    create correct csv_module & xlsx_module

--- src/csv_excel_module.py
+++ src/csv_excel_module.py
@@ -11,27 +11,28 @@ import openpyxl
 # так как целью является обработка и анализ информации,
 # а не её повторная запись в файл.
 
-# def csv_module(df)-> list[Any]:
-#     result_dict = []
-#     with open(df, newline='', encoding = 'utf-8') as file:
-#         reader = csv.DictReader(file)
-#         for row in reader:
-#             result_dict.append(row)
-#         return result_dict
-#
-# result = csv_module('../data/transactions.csv')
-# print(result)
-# print(type(result))
+def csv_module(df)-> list[dict]:
+    """Функция принимает файл в формате csv
+    и возвращает список словарей"""
+    result_dict = []
+    with open(df, newline='', encoding = 'utf-8') as file:
+        reader = csv.DictReader(file)
+        for row in reader:
+            result_dict.append(row)
+        return result_dict
 
-def excel_module(df, user_request):
+result = csv_module('../data/transactions.csv')
+print(result)
+print(type(result))
+
+def excel_module(df)-> list[dict]:
+    """Функция принимает файл в формате xlsx
+    и возвращает список словарей"""
     reader = pd.read_excel(df, engine="openpyxl")
-    result_2 = reader[user_request].to_dict()
+    result_2 = reader.to_dict(orient='records')
     return result_2
-# reader.to_dict(orient='records')
-# Если тебе нужно преобразовать весь DataFrame в словарь,
-# можешь использовать reader.to_dict(orient='records'),
-# чтобы получить список словарей
-result = excel_module('../data/transactions_excel.xlsx', 'state')
+
+result = excel_module('../data/transactions_excel.xlsx')
 print(result)
 print(type(result))
 
