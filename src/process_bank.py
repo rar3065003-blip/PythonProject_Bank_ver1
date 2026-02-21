bd4b5526dc6f161b25a4a52f3a62385b3ba4ffdb
Андрей Атаманов <rar3065003@gmail.com>
2026-02-21 19:41:39 +0700

    process bank rebuilded

--- src/process_bank.py
+++ src/process_bank.py
@@ -1,6 +1,8 @@
 import re
+from collections import Counter
 
-test_data = [
+
+# test_data = [
     {'id': '650703', 'state': 'EXECUTED', 'date': '2023-09-05T11:30:32Z',
      'amount': '16210', 'currency_name': 'Sol', 'currency_code': 'PEN',
      'from': 'Счет 58803664561298323391', 'to': 'Счет 39745660563456619397', 'description': 'Перевод организации'},
@@ -16,34 +18,46 @@ def process_bank_search(data:list[dict], search:str)->list[dict]:
     """ Функция принимает список банковских странзакций и возвращает
     список транзакций отфильтрованных по запросу"""
     search_result = []
-    pattern = f'^{re.escape(search)}$'
     for item in data:
-        if re.fullmatch(pattern, item['description']):
+        if re.search(search, item['description'], flags=re.IGNORECASE):
             search_result.append(item)
 
     return search_result
-
 # print(process_bank_search(test_data, 'Перевод с карты на карту'))
-
-
-
 
 def process_bank_operations(data:list[dict], categories:list)->dict: # categories это description
     """ Функция принимает список транзакций,
      а возвращает словарь, где отражается вид и количество запрошенных операций"""
-    result = {}
-    text_dict = ""
-    for dict in data:
-       for value in dict.values():
-           text_dict += str(value).lower()+ " "
+    matched_categories = []
+    compiled_patterns = {}
 
-    for word in categories:
-        escape_word = re.escape(word.lower())
-        pattern = r'\b' + escape_word +r'\b'
-        matches = re.findall(pattern, text_dict)
-        count = len(matches)
-        result[word] = count
+    for category in categories:
+         escaped_category = re.escape(category.lower())
+         pattern = r'\b' + escaped_category + r'\b'
+         compiled_patterns[category] = re.compile(pattern, re.IGNORECASE)
+
+    for transaction in data:
+        description = transaction.get('description', '').lower()
+        if not description:
+            continue
+
+        found_category = None
+
+        for category, pattern in compiled_patterns.items():
+            if pattern.search(description):
+                found_category = category
+                break
+
+        if found_category:
+            matched_categories.append(found_category)
+
+    counter = Counter(matched_categories)
+    result = dict(counter)
+
+    for category in categories:
+        if category not in result:
+            result[category] = 0
+
     return result
 
-
-print(process_bank_operations(test_data, ['Перевод организации', 'Перевод с карты на карту' ]))
+# print(process_bank_operations(test_data, ['Перевод организации', 'Перевод с карты на карту']))
